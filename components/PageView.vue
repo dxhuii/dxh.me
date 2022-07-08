4c311a5cf9cb7895cc723e76c7fd2a69a8f91466
效辉 <dxhuii@qq.com>
2022-07-09 02:15:30 +0800

    更新node版本

--- components/PageView.vue
+++ components/PageView.vue
@@ -2,10 +2,14 @@
 const { data } = await useFetch('/api/pageview')
 
 const time = useTimeAgo(computed(() => data.value.startAt))
+const year = useDateFormat(computed(() => data.value.startAt), 'YYYY')
 </script>
 
 <template>
   <div text-gray:80>
+    <span text="base" mr-2>
+      Dxh.me © {{ year }}
+    </span>
     <span font-500 text-gray>{{ data.pageview }}</span>
     页面次数 停留
     <span text-gray>{{ time }}</span>
